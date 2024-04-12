4d3fbfddda7b885160ca5006799e8ccef9578f3e
Adam Wentz <adam@adamwentz.com>
2024-04-12 10:10:26 -0700

    Add max seq len holo config

--- babylm/models/hf_holo/config.py
+++ babylm/models/hf_holo/config.py
@@ -9,11 +9,13 @@ class HFHoloConfig(PretrainedConfig):
         vocab_size: int = 50_257,
         model_dims: int = 128,
         num_hidden_layers: int = 2,
+        max_seq_len: int = 1024,
         **kwargs
     ):
         self.vocab_size = vocab_size
         self.model_dims = model_dims
         self.num_hidden_layers = num_hidden_layers
+        self.max_seq_len = max_seq_len
         super().__init__(**kwargs)
 
 
