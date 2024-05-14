b1a1965bbb0884c7628e66b8966a559648287214
Adam Wentz <adam@adamwentz.com>
2024-05-13 20:08:25 -0700

    use absolute import in these scripts

--- babylm/hf_run_clm.py
+++ babylm/hf_run_clm.py
@@ -57,7 +57,7 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
-from .hf_mup_trainer import MuPTrainer
+from babylm.hf_mup_trainer import MuPTrainer
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
