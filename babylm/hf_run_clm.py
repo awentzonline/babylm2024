5fa4dde5a67b1bf1a47e1989457c73bf38e9c8b0
Adam Wentz <adam@adamwentz.com>
2024-05-25 13:19:27 -0700

    throw out short blocks from dataset

--- babylm/hf_run_clm.py
+++ babylm/hf_run_clm.py
@@ -473,11 +473,14 @@ def main():
     def group_texts(examples):
         # Concatenate all texts.
         concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
+
+        orig_tl = total_length = len(concatenated_examples[list(examples.keys())[0]])
         # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
         # customize this part to your needs.
         if total_length >= block_size:
             total_length = (total_length // block_size) * block_size
+        elif total_length < block_size:
+            total_length = 0
         # Split by chunks of max_len.
         result = {
             k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
