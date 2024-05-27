be81356b4e02d2ab288247c1220aab15ed696eb9
Adam Wentz <adam@adamwentz.com>
2024-05-27 11:56:50 -0700

    add asymmetric focal loss

--- babylm/models/hf_holo/model.py
+++ babylm/models/hf_holo/model.py
@@ -312,6 +312,7 @@ class HFHolo(PreTrainedModel):
         self.loss_fn = {
             'xent': F.cross_entropy,
             'focal': focal_loss,
+            'asl': ASLSingleLabel(),
         }[config.loss]
 
         self.post_init()
@@ -560,6 +561,58 @@ def focal_loss(logits, targets, gamma=2.):
     focal_modulation = (1 - probs) ** gamma
     xent_loss = F.cross_entropy(logits, targets, reduction='none')
     return torch.mean(focal_modulation * xent_loss)
+
+
+class ASLSingleLabel(nn.Module):
+    '''
+    This loss is intended for single-label classification problems
+
+    From: https://github.com/lnsmith54/CFL/blob/main/OLTR/loss/asl_focal_loss.py
+    '''
+    def __init__(self, gamma_pos=0, gamma_neg=4, eps: float = 0.1, reduction='mean'):
+        super(ASLSingleLabel, self).__init__()
+
+        self.eps = eps
+        self.logsoftmax = nn.LogSoftmax(dim=-1)
+        self.targets_classes = []
+        self.gamma_pos = gamma_pos
+        self.gamma_neg = gamma_neg
+        self.reduction = reduction
+#        print("ASLSingleLabel: gamma_pos=", gamma_pos," gamma_neg=",gamma_neg, " eps=",eps)
+
+    def forward(self, inputs, target):
+        '''
+        "input" dimensions: - (batch_size,number_classes)
+        "target" dimensions: - (batch_size)
+        '''
+        num_classes = inputs.size()[-1]
+        log_preds = self.logsoftmax(inputs)
+        if len(list(target.size()))>1:
+            target = torch.argmax(target, 1)
+        self.targets_classes = torch.zeros_like(inputs).scatter_(1, target.long().unsqueeze(1), 1)
+
+        # ASL weights
+        targets = self.targets_classes
+        anti_targets = 1 - targets
+        xs_pos = torch.exp(log_preds)
+        xs_neg = 1 - xs_pos
+        xs_pos = xs_pos * targets
+        xs_neg = xs_neg * anti_targets
+        asymmetric_w = torch.pow(1 - xs_pos - xs_neg,
+                                 self.gamma_pos * targets + self.gamma_neg * anti_targets)
+        log_preds = log_preds * asymmetric_w
+
+        if self.eps > 0:  # label smoothing
+            self.targets_classes = self.targets_classes.mul(1 - self.eps).add(self.eps / num_classes)
+
+        # loss calculation
+        loss = - self.targets_classes.mul(log_preds)
+
+        loss = loss.sum(dim=-1)
+        if self.reduction == 'mean':
+            loss = loss.mean()
+
+        return loss
 
 
 HFHolo.register_for_auto_class("AutoModel")
