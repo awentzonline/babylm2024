49505598ee3831d3c1645d8a34f07b78545f72bb
Adam Wentz <adam@adamwentz.com>
2024-07-11 15:21:01 -0700

    and config

--- babylm/models/hf_holo/config.py
+++ babylm/models/hf_holo/config.py
@@ -7,17 +7,17 @@ class HFHoloConfig(PretrainedConfig):
     def __init__(
         self,
         vocab_size: int = 50_257,  # 50_304,  # gpt2 tokens padded (50257) to nearest multiple of 64
-        model_dims: int = 128,
+        model_dims: int = 1024,
         num_hidden_layers: int = 3,
         max_seq_len: int = 1024,
         learn_input_embs: bool = True,
         learn_output_embs: bool = True,
         attention_class: str = 'hrr',
-        initializer_range: float = 0.02,
+        initializer_range: float = 0.2,
         rezero: bool = False,
         loss: str = 'xent',
         use_norm_bias: bool = True,
-        num_heads: int = 8,
+        num_heads: int = 1,
         hrr_embedding: bool = False,
         position_embedding: str = 'learn',
         **kwargs
